7f17106b6ed5793a27bada9b5756b7d2447a359b
luizdivino <nandodivinodev@gmail.com>
2026-03-03 16:52:05 -0300

    Criei a função getDetails que recebe o nome do pokemon e a tipagem de informações do mesmo e retorna os dados correspondentes

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,5 +5,12 @@ export const api = axios.create({
     baseURL: 'https://pokeapi.co/api/v2/'
 })
 
-export const getPokemonList = async () => api.get<PokemonListResponse>('pokemon?limit=151');
-export const getPokemonDetails = (name: string) => api.get<PokemonDetailsResponse>(`pokemon/${name}`);
+export const getPokemonList = async () =>{
+    const responseList = await api.get<PokemonListResponse>('pokemon?limit=151');  
+    return responseList.data.results;
+}
+export const getPokemonDetails = async (name: string) => {
+    const responsePokemon = await api.get<PokemonDetailsResponse>(`pokemon/${name}`);
+    console.log(responsePokemon.data);
+    return responsePokemon.data;
+}
